5b8e230a645b05fdeacf31d4d10a0abf5e18ca5a
Farhan <67801321+Farhan687636@users.noreply.github.com>
2021-04-16 23:02:10 +0700

    Add files via upload

--- public/js/ruang-admin.js
+++ public/js/ruang-admin.js
@@ -1,3 +1,28 @@
+var countDate = new Date('May 13 2021 00:00:00').getTime();
+
+function newYear(){
+	var now = new Date().getTime();
+	gap = countDate - now;
+
+	var detik = 1000;
+	var menit = detik * 60;
+	var jam = menit * 60;
+	var hari = jam * 24;
+
+	var h = Math.floor(gap / (hari));
+	var j = Math.floor( (gap % (hari)) / (jam) );
+	var m = Math.floor( (gap % (jam))  / (menit) );
+	var d = Math.floor( (gap % (menit))  / (detik) );
+
+	document.getElementById('hari').innerText = h;
+	document.getElementById('jam').innerText = j;
+	document.getElementById('menit').innerText = m;
+	document.getElementById('detik').innerText = d;
+}
+
+setInterval( function(){
+	newYear();
+}, 1000);
 (function($) {
   "use strict"; // Start of use strict
 
